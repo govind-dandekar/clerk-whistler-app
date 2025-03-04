026cb45e61ec62205f9ec67e5594916afde48af2
Govind Dandekar <govind.dandekar@gmail.com>
2025-03-04 08:50:27 -0800

    comment and import clean-up

--- app/organization/page.jsx
+++ app/organization/page.jsx
@@ -1,6 +1,5 @@
-import { OrganizationSwitcher, OrganizationList } from '@clerk/nextjs'
+import { OrganizationList } from '@clerk/nextjs'
 
-// update links to take to forecast based on group membership
 export default function Organization(){
 	return (
 		<div className="-mt-16">
